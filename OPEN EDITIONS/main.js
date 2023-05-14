39644e6f8e995ad1ac62ae24492e5c8c85fa870a
kratuslengend <hoanghuynguyen20042007@gmail.com>
2023-05-14 11:27:34 +0700

    up

---
+++ OPEN EDITIONS/main.js
@@ -0,0 +1,19 @@
+const pi = 3.14;
+
+let a = 9;
+let b = -9;
+console.log(a + b);
+console.log(a - b)
+console.log(a*b)
+console.log(a**b)
+console.log(a/b)
+if (a == b) {
+  console.log("bằng nhau");
+} else {
+  console.log("không bằng nhau");
+}
+let c = 8;
+if(c%2==0){console.log("chẳn")}
+else{"lẻ"}
+if(b<=0){console.log("dương")}else{"âm"}
+console.log(Math.abs(b))
